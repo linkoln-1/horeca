f78acbe9ebef5f847cc52c129c6291484e0478a0
lincode <burrouws@bk.ru>
2024-06-04 21:27:35 +0300

    fix user queries

--- src/entities/user/user.queries.ts
+++ src/entities/user/user.queries.ts
@@ -24,22 +24,22 @@ export function useRegisterUserMutation() {
     })
 }
 
-export function useLoginUserMutation() {
-    const queryClient = useQueryClient()
-    const router = useRouter()
-
-    return useMutation({
-        mutationFn: api.authorizationControllerLogin,
-        onSuccess: async ({ data }) => {
-            userStore.getState().updateTokens({
-                accessToken: data.accessToken || null,
-                refreshToken: data.refreshToken || null,
-            })
-
-            router.push('/user')
-
-            await queryClient.invalidateQueries({ queryKey: ['user', 'me'] })
-        },
-    })
-}
+// export function useLoginUserMutation() {
+//     const queryClient = useQueryClient()
+//     const router = useRouter()
+//
+//     return useMutation({
+//         mutationFn: api.authorizationControllerLogin,
+//         onSuccess: async ({ data }) => {
+//             userStore.getState().updateTokens({
+//                 accessToken: data.accessToken || null,
+//                 refreshToken: data.refreshToken || null,
+//             })
+//
+//             router.push('/user')
+//
+//             await queryClient.invalidateQueries({ queryKey: ['user', 'me'] })
+//         },
+//     })
+// }
 // hello from change
