b2f4cc2e301546112d3efe5cd9ece7a4e7232a61
lincode <burrouws@bk.ru>
2024-09-22 23:37:27 +0300

    fix small bags

--- src/features/favoriteProviders/favoriteProviders.modal.tsx
+++ src/features/favoriteProviders/favoriteProviders.modal.tsx
@@ -1,14 +1,19 @@
-import { Text, Flex, Button, Paper } from "@mantine/core"
+import { Text, Flex, Button, Paper } from '@mantine/core'
 
 export function FavoriteProvidersModal() {
-    return(
+    return (
         <Paper p='md'>
-            <Text fw='500' mb='20px' size="xl" ta='center'>
-                Вы уверены, что хотите удалить из избранного? Отменить это действие будет невозможно
+            <Text fw='500' mb='20px' size='xl' ta='center'>
+                Вы уверены, что хотите удалить из избранного? Отменить это
+                действие будет невозможно
             </Text>
-            <Flex justify='center' gap='40px'>
-                <Button size="md" radius='xl' w='40%' bg='indigo'>Нет</Button>
-                <Button size="md" radius='xl' w='40%' bg='pink.7'>Да</Button>
+            <Flex justify='center' gap='md'>
+                <Button size='md' radius='xl' w='40%' bg='indigo'>
+                    Нет
+                </Button>
+                <Button size='md' radius='xl' w='40%' bg='pink.7'>
+                    Да
+                </Button>
             </Flex>
         </Paper>
     )
--- src/views/favoriteProvider/favoriteProvider.ui.tsx
+++ src/views/favoriteProvider/favoriteProvider.ui.tsx
@@ -1,10 +1,16 @@
-'use client';
+'use client'
 
-import { FavoriteProvidersModal } from '@/features/favoriteProviders/favoriteProviders.modal';
-import { Image, Flex, Paper, Text, Stack } from '@mantine/core';
-import { modals } from '@mantine/modals';
-import Link from 'next/link';
-
+import { FavoriteProvidersModal } from '@/features/favoriteProviders/favoriteProviders.modal'
+import {
+    Image as MantineImage,
+    Flex,
+    Paper,
+    Text,
+    Stack,
+    Button,
+} from '@mantine/core'
+import { modals } from '@mantine/modals'
+import Link from 'next/link'
 
 type Supplier = {
     img: string
@@ -52,25 +58,43 @@ const suppliers: Supplier[] = [
 export function FavoriteProviderViews() {
     return (
         <Flex direction='column' gap='40px' p='md'>
-            {suppliers.map(suplier => {
-                return(
-                    <Flex className='supplier' w='100%'>
-                        <Paper w='100%' px={100} py={63} withBorder shadow="sm" radius="lg" p="xl">
+            {suppliers.map((suplier, index) => {
+                return (
+                    <Flex className='supplier' w='100%' key={index}>
+                        <Paper
+                            w='100%'
+                            px={100}
+                            py={63}
+                            withBorder
+                            shadow='sm'
+                            radius='lg'
+                            p='xl'
+                        >
                             <Flex className='supplierWrapper'>
                                 <Flex w='75%'>
-                                    <Image fit='cover' mr='100px' w={180} h={180} radius='md' src={suplier.img}></Image>
+                                    <MantineImage
+                                        fit='cover'
+                                        mr='100px'
+                                        w={180}
+                                        h={180}
+                                        radius='md'
+                                        src={suplier.img}
+                                    />
 
                                     <Stack py='20px' justify='space-between'>
                                         <Flex>
-
                                             <Text size='20px'>
                                                 <Text fw={500} mr='5px' span>
                                                     Наименование:
                                                 </Text>
 
-                                                <Text display='inline' size='20px'>{suplier.name}</Text>
+                                                <Text
+                                                    display='inline'
+                                                    size='20px'
+                                                >
+                                                    {suplier.name}
+                                                </Text>
                                             </Text>
-                                            
                                         </Flex>
                                         <Flex>
                                             <Text size='20px'>
@@ -78,39 +102,67 @@ export function FavoriteProviderViews() {
                                                     Рейтинг:
                                                 </Text>
 
-                                                <Text display='inline' size='20px'>{suplier.rating} / 5</Text>
+                                                <Text
+                                                    display='inline'
+                                                    size='20px'
+                                                >
+                                                    {suplier.rating} / 5
+                                                </Text>
                                             </Text>
                                         </Flex>
                                         <Flex>
-                                            <Text size='20px' >
+                                            <Text size='20px'>
                                                 <Text fw={500} mr='5px' span>
                                                     Категории товаров:
                                                 </Text>
-                                                <Text lh='md' display='inline' size='20px'>
-                                                    {suplier.categories.map(category => {
-                                                        return(
-                                                            category + '; '
-                                                        )
-                                                    })}
+                                                <Text
+                                                    lh='md'
+                                                    display='inline'
+                                                    size='20px'
+                                                >
+                                                    {suplier.categories.map(
+                                                        category => {
+                                                            return (
+                                                                category + '; '
+                                                            )
+                                                        }
+                                                    )}
                                                 </Text>
                                             </Text>
-                                            
                                         </Flex>
                                     </Stack>
-
                                 </Flex>
-                                
-                                <Stack className='flex-1' justify='center' gap='36px' align='center'>
-                                    <Text href='/provider/chat' component={Link} size='20px' className='cursor-pointer' c='indigo'>Открыть список чатов</Text>
-                                    <Text onClick={handleModal} size='20px' className='cursor-pointer' c='pink.7'>Удалить из избранного</Text>
+
+                                <Stack
+                                    className='flex-1'
+                                    justify='center'
+                                    gap='36px'
+                                    align='center'
+                                >
+                                    <Button
+                                        href='/provider/chat'
+                                        component={Link}
+                                        size='20px'
+                                        className='cursor-pointer'
+                                        c='indigo'
+                                        variant='transparent'
+                                    >
+                                        Открыть список чатов
+                                    </Button>
+                                    <Text
+                                        onClick={handleModal}
+                                        size='20px'
+                                        className='cursor-pointer'
+                                        c='pink.7'
+                                    >
+                                        Удалить из избранного
+                                    </Text>
                                 </Stack>
                             </Flex>
                         </Paper>
                     </Flex>
                 )
             })}
-            
-
         </Flex>
     )
 }
@@ -118,7 +170,7 @@ export function FavoriteProviderViews() {
 function handleModal() {
     modals.open({
         centered: true,
-        modalId: 'product',
+        modalId: 'favoriteProvider',
         radius: 'lg',
         size: 'xl',
         children: <FavoriteProvidersModal />,
