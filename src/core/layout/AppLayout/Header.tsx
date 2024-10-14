7133df4df13001ed26116352484d56bf955bd6ac
f-murphy <100728686+f-murphy@users.noreply.github.com>
2024-10-14 19:00:50 +0300

    change header

--- src/core/layout/AppLayout/Header.tsx
+++ src/core/layout/AppLayout/Header.tsx
@@ -6,6 +6,8 @@ import {
     Image as MantineImage,
     Loader,
     Paper,
+    useCombobox,
+    Menu,
 } from '@mantine/core'
 import Link from 'next/link'
 
@@ -15,7 +17,9 @@ import { useBreakpoint } from '@/shared/hooks/useBreakpoint'
 export function Header() {
     const isMobile = useBreakpoint('sm')
     const user = useUserStore(state => state.user)
-
+    const combobox = useCombobox({
+        onDropdownClose: () => combobox.resetSelectedOption(),
+    })
     const { clearTokens: logOut } = useUserStore(state => state)
 
     if (!user) return <Loader />
@@ -32,7 +36,16 @@ export function Header() {
                     </Link>
 
                     {!isMobile && (
-                        <Flex gap='md' align='center'>
+                        <Flex gap='sm' align='center'>
+                            <Button
+                                component={Link}
+                                href={'#'}
+                                color='dark'
+                                size='compact-sm'
+                                variant='transparent'
+                            >
+                                Реклама
+                            </Button>
                             <Button
                                 component={Link}
                                 href={'/help'}
@@ -42,14 +55,45 @@ export function Header() {
                             >
                                 Помощь
                             </Button>
-                            <Button
-                                color='dark'
-                                size='compact-sm'
-                                variant='transparent'
-                                onClick={logOut}
-                            >
-                                Выйти
-                            </Button>
+                            <Menu>
+                                <Menu.Target>
+                                    <Button
+                                        variant='transparent'
+                                        className='gap-8'
+                                    >
+                                        <Flex gap='sm' align='center'>
+                                            <div>
+                                                <MantineImage
+                                                    src='/assets/icons/profile.svg'
+                                                    alt='Horeka profile'
+                                                />
+                                            </div>
+                                            <div className='text-black'>
+                                                Профиль
+                                            </div>
+                                            <div>
+                                                <MantineImage
+                                                    src='/assets/icons/arrow.svg'
+                                                    alt='Horeka arrow'
+                                                />
+                                            </div>
+                                        </Flex>
+                                    </Button>
+                                </Menu.Target>
+
+                                <Menu.Dropdown>
+                                    <Menu.Item>
+                                        <Button
+                                            color='dark'
+                                            size='compact-sm'
+                                            variant='transparent'
+                                            onClick={logOut}
+                                        >
+                                            Выйти
+                                        </Button>
+                                    </Menu.Item>
+                                </Menu.Dropdown>
+                            </Menu>
                         </Flex>
                     )}
                 </Flex>
