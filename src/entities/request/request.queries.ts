2747b6a16896ad58c0f56ab9beee54f3fc3dbe08
lincode <burrouws@bk.ru>
2024-10-17 18:26:56 +0300

    add application queries

--- src/entities/request/request.queries.ts
+++ src/entities/request/request.queries.ts
@@ -2,6 +2,14 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import { api } from '@/shared/lib/horekaApi'
 import { HorecaRequestCreateDto } from '@/shared/lib/horekaApi/Api'
+import { useCustomQuery } from '@/shared/lib/reactQuery/useCustomQuery'
+
+export function useGetRequestQuery() {
+    return useCustomQuery({
+        queryKey: ['request, horeca'],
+        queryFn: () => api.horecaRequestsControllerFindAll(),
+    })
+}
 
 export function useCreateRequestMutation() {
     const queryClient = useQueryClient()
@@ -11,7 +19,9 @@ export function useCreateRequestMutation() {
             api.horecaRequestsControllerCreate(data),
 
         onSuccess: async ({ data }) => {
-            await queryClient.invalidateQueries({ queryKey: ['user', 'me'] })
+            await queryClient.invalidateQueries({
+                queryKey: ['request, horeca'],
+            })
         },
     })
 }
--- src/views/applications/applications.ui.tsx
+++ src/views/applications/applications.ui.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 
+import { requestQueries } from '@/entities/request'
 import {
     Badge,
     Box,
@@ -9,6 +10,7 @@ import {
     Divider,
     Flex,
     Grid,
+    Loader,
     SegmentedControl,
     Text,
 } from '@mantine/core'
@@ -20,132 +22,134 @@ import { useBreakpoint } from '@/shared/hooks/useBreakpoint'
 export function ApplicationsViews() {
     const [activeTab, setActiveTab] = useState(applications[0].label)
     const isMobile = useBreakpoint('sm')
+
     const handleTabChange = (tab: string) => {
         setActiveTab(tab)
     }
-    const orders = [
-        {
-            id: '82725225279 от 20.07.2023',
-            status: 'В работе',
-            message: '1 новое сообщение в чате',
-            name: 'Палтус',
-            address: 'ул. Белокобыльская, 69',
-            weight: '50 кг',
-            deliveryDate: '12.08.2023',
-            success: true,
-        },
-        {
-            id: '82725225279 от 20.06.2023',
-            status: 'Ожидает подтверждения (чат недоступен)',
-            name: 'Палтус',
-            address: 'ул. Белокобыльская, 69',
-            weight: '50 кг',
-            deliveryDate: '12.08.2023',
-            success: true,
-        },
-        {
-            id: '82725225279 от 20.06.2023',
-            status: 'Завершена (успешно)',
-            name: 'Палтус',
-            address: 'ул. Белокобыльская, 69',
-            weight: '50 кг',
-            deliveryDate: '12.08.2023',
-            success: true,
-        },
-        {
-            id: '82725225279 от 20.06.2023',
-            status: 'Завершена (неуспешно)',
-            name: 'Палтус',
-            address: 'ул. Белокобыльская, 69',
-            weight: '50 кг',
-            deliveryDate: '12.08.2023',
-            success: false,
-        },
-    ]
+    const { data: requests } = requestQueries.useGetRequestQuery()
 
+    if (!requests) return <Loader />
+
+    // TODO тут насколько я понял запрос не по типам возвращает данные, после как бэк вернется, нужно проверить
     return (
         <Flex direction='column' gap='md'>
-            <Flex gap='md' align='center'>
-                <SegmentedControl
-                    fullWidth
-                    onChange={handleTabChange}
-                    value={activeTab}
-                    color='blue'
-                    data={[
-                        applications[0].label,
-                        applications[1].label,
-                        applications[2].label,
-                    ]}
-                    orientation={isMobile ? 'vertical' : 'horizontal'}
-                />
-            </Flex>
+            {!requests.data ? (
+                <Flex justify='center' align='center'>
+                    <Text size='lg' c='gray'>
+                        Сейчас у вас созданных заявок нет
+                    </Text>
+                </Flex>
+            ) : (
+                <>
+                    <Flex gap='md' align='center'>
+                        <SegmentedControl
+                            fullWidth
+                            onChange={handleTabChange}
+                            value={activeTab}
+                            color='blue'
+                            data={[
+                                applications[0].label,
+                                applications[1].label,
+                                applications[2].label,
+                            ]}
+                            orientation={isMobile ? 'vertical' : 'horizontal'}
+                        />
+                    </Flex>
+                    <Grid>
+                        {requests.data?.map((order, index) => (
+                            <Grid.Col
+                                span={{
+                                    base: 12,
+                                    md: 6,
+                                }}
+                                key={index}
+                            >
+                                <Card
+                                    shadow='sm'
+                                    padding='lg'
+                                    radius='lg'
+                                    withBorder
+                                >
+                                    <Text fw={500}>№ {order.id}</Text>
 
-            <Grid>
-                {orders.map((order, index) => (
-                    <Grid.Col
-                        span={{
-                            base: 12,
-                            md: 6,
-                        }}
-                        key={index}
-                    >
-                        <Card shadow='sm' padding='lg' radius='lg' withBorder>
-                            <Text fw={500}>№ {order.id}</Text>
-
-                            <Flex align='center' justify='space-between'>
-                                <Flex align='center' gap='md'>
-                                    <Badge
-                                        color={order.success ? 'green' : 'red'}
-                                        variant='light'
+                                    <Flex
+                                        align='center'
+                                        justify='space-between'
                                     >
-                                        {order.status}
-                                    </Badge>
-                                    {order.message && (
-                                        <Text
-                                            size='sm'
-                                            c='blue'
-                                            style={{ marginBottom: 5 }}
+                                        <Flex align='center' gap='md'>
+                                            <Badge
+                                                // color={order ? 'green' : 'red'}
+                                                variant='light'
+                                            >
+                                                {/*{order.}*/}
+                                                тут будут статусы
+                                            </Badge>
+                                            {order.comment && (
+                                                <Text
+                                                    size='sm'
+                                                    c='blue'
+                                                    style={{ marginBottom: 5 }}
+                                                >
+                                                    {order.comment}{' '}
+                                                </Text>
+                                            )}
+                                        </Flex>
+
+                                        <Box>
+                                            <IconMessage size={30} />
+                                        </Box>
+                                    </Flex>
+                                    <Divider
+                                        orientation='horizontal'
+                                        mt='md'
+                                        mb='md'
+                                    />
+
+                                    <Flex direction='column' gap='md'>
+                                        <Text size='sm'>
+                                            <strong>
+                                                Информация по вашему заказу
+                                            </strong>
+                                        </Text>
+
+                                        <Flex
+                                            justify='space-between'
+                                            align='center'
                                         >
-                                            {order.message}{' '}
-                                        </Text>
-                                    )}
-                                </Flex>
+                                            {order.items.map((item, index) => (
+                                                <Flex
+                                                    direction='column'
+                                                    gap='md'
+                                                    key={index}
+                                                >
+                                                    <Text size='sm'>
+                                                        Название: {order.name}
+                                                    </Text>
+                                                    <Text size='sm'>
+                                                        Количество:{' '}
+                                                        {item.amount}
+                                                    </Text>
+                                                </Flex>
+                                            ))}
 
-                                <Box>
-                                    <IconMessage size={30} />
-                                </Box>
-                            </Flex>
-                            <Divider orientation='horizontal' mt='md' mb='md' />
-
-                            <Flex direction='column' gap='md'>
-                                <Text size='sm'>
-                                    <strong>Информация по вашему заказу</strong>
-                                </Text>
-
-                                <Flex justify='space-between' align='center'>
-                                    <Flex direction='column' gap='md'>
-                                        <Text size='sm'>
-                                            Название: {order.name}
-                                        </Text>
-                                        <Text size='sm'>
-                                            Количество: {order.weight}
-                                        </Text>
+                                            <Flex direction='column' gap='md'>
+                                                <Text size='sm'>
+                                                    Адрес доставки:{' '}
+                                                    {order.address}
+                                                </Text>
+                                                <Text size='sm'>
+                                                    Дата доставки:{' '}
+                                                    {order.deliveryTime}
+                                                </Text>
+                                            </Flex>
+                                        </Flex>
                                     </Flex>
-
-                                    <Flex direction='column' gap='md'>
-                                        <Text size='sm'>
-                                            Адрес доставки: {order.address}
-                                        </Text>
-                                        <Text size='sm'>
-                                            Дата доставки: {order.deliveryDate}
-                                        </Text>
-                                    </Flex>
-                                </Flex>
-                            </Flex>
-                        </Card>
-                    </Grid.Col>
-                ))}
-            </Grid>
+                                </Card>
+                            </Grid.Col>
+                        ))}
+                    </Grid>
+                </>
+            )}
         </Flex>
     )
 }
